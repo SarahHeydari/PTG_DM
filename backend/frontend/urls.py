97b7dc6ddceb5259b4e81e282297dce12929633a
Sara Heydari <sara80heydari@gmail.com>
2026-02-09 14:37:18 +0330

    pre role profile

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -4,5 +4,10 @@ from .views import *
 urlpatterns = [
     path("fire/", fire_dashboard, name="fire-dashboard"),
     path("", dashboard, name="dashboard"),
-    path("login/", login_view, name="login"),
+    path("login/", login_page, name="login"),
+    path("register/", register_page, name="register"),
+    path("profile/", profile_router, name="profile_router"),
+    path("profile/manager/", manager_profile, name="manager_profile"),
+    path("profile/expert/", expert_profile, name="expert_profile"),
+    path("profile/admin/", admin_profile, name="admin_profile"),
 ]
--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -7,5 +7,25 @@ def fire_dashboard(request):
 def dashboard(request):
     return render(request, "frontend/dashboard.html")
 
-def login_view(request):
+def register_page(request):
+    return render(request, "frontend/register.html")
+
+
+def login_page(request):
     return render(request, "frontend/login.html")
+
+
+def profile_router(request):
+    return render(request, "frontend/profile_router.html")
+
+def manager_profile(request):
+    return render(request, "frontend/manager_profile.html")
+
+def expert_profile(request):
+    return render(request, "frontend/expert_profile.html")
+
+def admin_profile(request):
+    return render(request, "frontend/admin_profile.html")
+
+
+
